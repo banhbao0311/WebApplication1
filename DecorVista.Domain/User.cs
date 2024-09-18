52d70b1185aa957cc77fb7f3f5cfb0c22690461b
banhbao0311 <nhan.nguyenthanh0311@gmail.com>
2024-09-18 11:54:52 +0700

    Add project files.

---
+++ DecorVista.Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DecorVista.Domain
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string username { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public UserDetails userdetails { get; set; } = new UserDetails();
+        public InteriorDesigner interiordesigner { get; set; } = new InteriorDesigner();
+
+        public List<Consultation> consultations { get; set; } = new List<Consultation>();
+
+        public List<Review> reviews { get; set; } = new List<Review>();
+        public List<Cart> carts { get; set; } = new List<Cart>();
+        public List<Order> orders { get; set; } = new List<Order>();
+        public List<Subcribe> subcribes { get; set; } = new List<Subcribe>();
+    }
+}
---
+++ WebApplication1/Migrations/20240917175705_InitialCreate.cs
@@ -0,0 +1,430 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Functionality",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Functionality", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomType",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomType", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    product_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    productname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    functionality_id = table.Column<int>(type: "int", nullable: false),
+                    brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<float>(type: "real", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    imageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Products_Functionality_functionality_id",
+                        column: x => x.functionality_id,
+                        principalTable: "Functionality",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gallery",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gallery_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<bool>(type: "bit", nullable: false),
+                    room_type_id = table.Column<int>(type: "int", nullable: false),
+                    imageName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gallery", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Gallery_RoomType_room_type_id",
+                        column: x => x.room_type_id,
+                        principalTable: "RoomType",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InteriorDesigners",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    yearsofexperience = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    specialization = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    portfolio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    daywork = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InteriorDesigners", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_InteriorDesigners_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    total = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Order_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetails", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_UserDetails_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    product_id = table.Column<int>(type: "int", nullable: false),
+                    quanity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Cart_Products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "Products",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Cart_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GalleryDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gallery_id = table.Column<int>(type: "int", nullable: false),
+                    product_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GalleryDetails", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_GalleryDetails_Gallery_gallery_id",
+                        column: x => x.gallery_id,
+                        principalTable: "Gallery",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GalleryDetails_Products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "Products",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subcribe",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gallery_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcribe", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Subcribe_Gallery_gallery_id",
+                        column: x => x.gallery_id,
+                        principalTable: "Gallery",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Subcribe_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    designer_id = table.Column<int>(type: "int", nullable: false),
+                    scheduled_datetime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    notes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultations", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Consultations_InteriorDesigners_designer_id",
+                        column: x => x.designer_id,
+                        principalTable: "InteriorDesigners",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Consultations_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    product_id = table.Column<int>(type: "int", nullable: false),
+                    designer_id = table.Column<int>(type: "int", nullable: false),
+                    comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_InteriorDesigners_designer_id",
+                        column: x => x.designer_id,
+                        principalTable: "InteriorDesigners",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Reviews_Products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "Products",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_product_id",
+                table: "Cart",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_user_id",
+                table: "Cart",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultations_designer_id",
+                table: "Consultations",
+                column: "designer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultations_user_id",
+                table: "Consultations",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gallery_room_type_id",
+                table: "Gallery",
+                column: "room_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GalleryDetails_gallery_id",
+                table: "GalleryDetails",
+                column: "gallery_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GalleryDetails_product_id",
+                table: "GalleryDetails",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InteriorDesigners_user_id",
+                table: "InteriorDesigners",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_user_id",
+                table: "Order",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_functionality_id",
+                table: "Products",
+                column: "functionality_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_designer_id",
+                table: "Reviews",
+                column: "designer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_product_id",
+                table: "Reviews",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_user_id",
+                table: "Reviews",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcribe_gallery_id",
+                table: "Subcribe",
+                column: "gallery_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcribe_user_id",
+                table: "Subcribe",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDetails_user_id",
+                table: "UserDetails",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cart");
+
+            migrationBuilder.DropTable(
+                name: "Consultations");
+
+            migrationBuilder.DropTable(
+                name: "GalleryDetails");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Subcribe");
+
+            migrationBuilder.DropTable(
+                name: "UserDetails");
+
+            migrationBuilder.DropTable(
+                name: "InteriorDesigners");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Gallery");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Functionality");
+
+            migrationBuilder.DropTable(
+                name: "RoomType");
+        }
+    }
+}
